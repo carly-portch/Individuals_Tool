13dc089e4089e83d2d8e30683ff98a8aeebd3944
carly-portch <carly.portch@outlook.com>
2024-10-02 16:53:18 -0400

    Update individuals_tool.py

--- individuals_tool.py
+++ individuals_tool.py
@@ -6,7 +6,7 @@ import pandas as pd
 # Function to calculate user's age
 def calculate_age(birthdate):
     today = date.today()
-    age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthday.day))
+    age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
     return age
 
 # Function to visualize income distribution into buckets
